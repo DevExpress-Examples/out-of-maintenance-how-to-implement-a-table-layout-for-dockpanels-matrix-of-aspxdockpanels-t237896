bb6f01346ece74a4b0c8f9b25aa1a06308629d8c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to implement a table layout for DockPanels (Matrix of ASPxDockPanels)
+
+
+<p>This example demonstrates how to arrange DockPanels as a 4x4 matrix (table). There are 16 DockPanels in the example. Any panel can be moved between two other DockPanles. All other panels will be correctly moved on to keep the 4x4 matrix dimension.</p>
+<strong>See also</strong>: <a href="https://www.devexpress.com/Support/Center/p/E4317">How to implement a table layout for DockPanels (Matrix of DockPanels)</a>
+
+<br/>
+
+
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,42 @@
+﻿Imports DevExpress.Web
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+        Dim columnNumber As Integer = 0
+        Dim zoneIndex As Integer = -1
+        For index As Integer = 0 To 15
+            If index Mod 4 = 0 Then
+                zoneIndex += 1
+                columnNumber = 0
+            End If
+            columnNumber += 1
+            Dim panel As New ASPxDockPanel()
+            panel.ID = String.Format("panel{0}", index)
+            Form.Controls.Add(panel)
+            panel.OwnerZoneUID = String.Format("zone{0}", zoneIndex)
+            panel.Width = 200
+            panel.Height = 200
+            panel.MaxHeight = 200
+            panel.VisibleIndex = columnNumber
+            panel.AllowedDockState = AllowedDockState.DockedOnly
+            panel.HeaderText = String.Format("Panel{0}", index)
+        Next index
+        For index As Integer = 0 To 3
+            Dim zone As New ASPxDockZone()
+            zone.ID = String.Format("zone{0}", index)
+            Form.Controls.Add(zone)
+            zone.Width = 800
+            zone.Height = 200
+            zone.Orientation = DockZoneOrientation.Horizontal
+            zone.ZoneUID = String.Format("zone{0}", index)
+        Next index
+    End Sub
+End Class
